783b303e905976be439ee621c23040e542658c8d
liuqi <70128222+l1uqi@users.noreply.github.com>
2023-12-27 15:02:34 +0800

    feat: 移除无用文件

--- src/config/env.rs
+++
@@ -1,9 +0,0 @@
-use clap::Parser;
-
-#[derive(Debug, Parser)]
-pub struct DingTalkConfig {
-    #[clap(required = true, env)]
-    pub dding_url: String,
-    #[clap(required = true, env)]  
-    pub dding_title: String,
-}
